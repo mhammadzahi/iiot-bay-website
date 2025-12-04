2826b5432bf6572fb861726052bcd1d89b2c26a1
Mohamed <mohamedzahi33@gmail.com>
2025-12-04 22:45:02 +0400

    .

--- functions/database.py
+++ functions/database.py
@@ -2,7 +2,7 @@ import sqlite3, os
 # from typing import List, Dict
 
 
-DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'database.db')
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'iiot_bay_database.db')
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
